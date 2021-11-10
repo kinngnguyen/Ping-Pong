053e52c54a06742672bc69ab82377a13ab8bf1ec
kinngnguyen <kinnguyennov11@gmail.com>
2021-11-10 20:38:07 +0700

    Update, Blit_image

--- Ping Pong.py
+++ Ping Pong.py
@@ -1,28 +1,33 @@
 from pygame import *
 
 # music
-mixer.init()
-mixer.music.load('space.ogg')
-mixer.music.play()
+# mixer.init()
+# mixer.music.load('space.ogg')
+# mixer.music.play()
 
 #create game window
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
-display.set_caption("Water-color-BG.jpeg")
-background = transform.scale(image.load("galaxy.jpg"), (700, 500))
+display.set_caption("Ping Pong")
+background = transform.scale(image.load("Water-color-BG.jpeg"), (700, 500))
 
 # Game Sprite
 class GameSprite(sprite.Sprite):
-    def __init__(self, image, x, y, width, height, speed):
+    def __init__(self, img, x, y, width, height, speed):
         super().__init__()
-        self.image = transform.scale(image.load(image), (width, height))
-        self.speed = speed
+        self.image = transform.scale(image.load(img), (width, height))
+        self.speed = 2
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-    def reset(self):
+        self.way_ball = randint(1, 4)
+    def blit_image(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Ball(GameSprite):
+    def update(self):
+        pass
 
 class Paddel_1(GameSprite):
     def update(self):
@@ -40,10 +45,6 @@ class Paddel_2(GameSprite):
         if keys[K_W] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
 
-class Ball(GameSprite):
-    def update(self):
-        pass
-
 ball = Ball("Ping-Pong-Ball.png", 330, 230, 40, 40, 5)
 
 paddel_1 = Paddel_1("Black Line", 20, 20, 75, 90, 5)
@@ -56,6 +57,17 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
- 
+
+    window.blit(background(0, 0))
+
+    paddel_1.blit_image()
+    paddel_2.blit_image()
+
+    paddel_1.update()
+    paddel_2.update()
+
+    ball.update()
+
+
 
 display.update()
