30ea21dec3ba6d6d5c86ef6f140b5020c077892e
kinngnguyen <kinnguyennov11@gmail.com>
2021-11-10 19:56:39 +0700

    Music, Gamesprite, Paddel

--- Ping Pong.py
+++ Ping Pong.py
@@ -1,1 +1,43 @@
-print("Hello")
+from pygame import *
+
+# music
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+
+#create game window
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("Ping Pong")
+background = transform.scale(image.load("galaxy.jpg"), (700, 500))
+
+# Game Sprite
+class GameSprite(sprite.Sprite):
+    def __init__(self, image, x, y, width, height, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(image), (width, height))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Paddel_1(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_DOWN] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_UP] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+class Paddel_2(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_S] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_W] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+display.update()
