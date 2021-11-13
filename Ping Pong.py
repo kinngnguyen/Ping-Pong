e009ca6066a93af3e7e6eeb4da5b52681fc39ad5
kinngnguyen <kinnguyennov11@gmail.com>
2021-11-13 16:43:19 +0700

    ball movement, black line crop

--- Ping Pong.py
+++ Ping Pong.py
@@ -45,12 +45,22 @@ class Paddel_2(GameSprite):
         if keys[K_s] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
 ball = Ball("Ping-Pong-Ball.png", 330, 230, 40, 40, 5)
 
-paddel_2 = Paddel_2("Black line.png", 20, 20, 75, 90, 6)
-paddel_1 = Paddel_1("Black line.png", 600, 400, 75, 90, 6)
+paddel_2 = Paddel_2("Black line.png", 20, 20, 25, 90, 6)
+paddel_1 = Paddel_1("Black line.png", 600, 400, 25, 90, 6)
 clock = time.Clock()
 game = True
+
+speed_x = 3
+speed_y = 3
+finish = False
+
 while game:
     keys = key.get_pressed()
     clock.tick(60)
@@ -58,14 +68,41 @@ while game:
         if e.type == QUIT:
             game = False
 
-    window.blit(background, (0, 0))
+    if finish != True:
 
-    paddel_1.blit_image()
-    paddel_2.blit_image()
+        window.blit(background, (0, 0))
 
-    paddel_1.update()
-    paddel_2.update()
+        paddel_1.update()
+        paddel_2.update()
 
-    ball.update()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        # paddle collision
+        if sprite.collide_rect(paddel_1, ball) or sprite.collide_rect(paddel_2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        # top/bottom of screen 
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        # player 1 lost
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200) )
+            game_over = True
+
+        # player 2 lost
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+        paddel_1.blit_image()
+        paddel_2.blit_image()
+        ball.blit_image()
+    
+        ball.update()
 
     display.update()
